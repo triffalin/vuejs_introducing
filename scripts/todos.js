c6a91715722adedc06b9a975124d0cea25d7b36c
Alin Triff <triffalin@gmail.com>
2023-10-28 19:02:56 +0300

    listened to events and updated data and the dom

--- scripts/todos.js
+++ scripts/todos.js
@@ -4,156 +4,156 @@ const todosListElement = document.getElementById('todos-list');
 let editedTodoElement;
 
 async function loadTodos() {
-  let response;
-  try {
-    response = await fetch('http://localhost:3000/todos');
-  } catch (error) {
-    alert('Something went wrong!');
-    return;
-  }
+	let response;
+	try {
+		response = await fetch('http://localhost:3000/todos');
+	} catch (error) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  if (!response.ok) {
-    alert('Something went wrong!');
-    return;
-  }
+	if (!response.ok) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  const responseData = await response.json();
-  const todos = responseData.todos;
+	const responseData = await response.json();
+	const todos = responseData.todos;
 
-  for (const todo of todos) {
-    createTodoListItem(todo.text, todo.id);
-  }
+	for (const todo of todos) {
+		createTodoListItem(todo.text, todo.id);
+	}
 }
 
 function createTodoListItem(todoText, todoId) {
-  const newTodoItemElement = document.createElement('li');
-  newTodoItemElement.dataset.todoid = todoId; // data-todoid
+	const newTodoItemElement = document.createElement('li');
+	newTodoItemElement.dataset.todoid = todoId; // data-todoid
 
-  const todoTextElement = document.createElement('p');
-  todoTextElement.textContent = todoText;
+	const todoTextElement = document.createElement('p');
+	todoTextElement.textContent = todoText;
 
-  const editTodoButtonElement = document.createElement('button');
-  editTodoButtonElement.textContent = 'Edit';
-  editTodoButtonElement.addEventListener('click', startTodoEditing);
+	const editTodoButtonElement = document.createElement('button');
+	editTodoButtonElement.textContent = 'Edit';
+	editTodoButtonElement.addEventListener('click', startTodoEditing);
 
-  const deleteTodoButtonElement = document.createElement('button');
-  deleteTodoButtonElement.textContent = 'Delete';
-  deleteTodoButtonElement.addEventListener('click', deleteTodo);
+	const deleteTodoButtonElement = document.createElement('button');
+	deleteTodoButtonElement.textContent = 'Delete';
+	deleteTodoButtonElement.addEventListener('click', deleteTodo);
 
-  const todoActionsWrapperElement = document.createElement('div');
-  todoActionsWrapperElement.appendChild(editTodoButtonElement);
-  todoActionsWrapperElement.appendChild(deleteTodoButtonElement);
+	const todoActionsWrapperElement = document.createElement('div');
+	todoActionsWrapperElement.appendChild(editTodoButtonElement);
+	todoActionsWrapperElement.appendChild(deleteTodoButtonElement);
 
-  newTodoItemElement.appendChild(todoTextElement);
-  newTodoItemElement.appendChild(todoActionsWrapperElement);
+	newTodoItemElement.appendChild(todoTextElement);
+	newTodoItemElement.appendChild(todoActionsWrapperElement);
 
-  todosListElement.appendChild(newTodoItemElement);
+	todosListElement.appendChild(newTodoItemElement);
 }
 
 async function createTodo(todoText) {
-  let response;
+	let response;
 
-  try {
-    response = await fetch('http://localhost:3000/todos', {
-      method: 'POST',
-      body: JSON.stringify({
-        text: todoText,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (error) {
-    alert('Something went wrong!');
-    return;
-  }
+	try {
+		response = await fetch('http://localhost:3000/todos', {
+			method: 'POST',
+			body: JSON.stringify({
+				text: todoText,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (error) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  if (!response.ok) {
-    alert('Something went wrong!');
-    return;
-  }
+	if (!response.ok) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  const responseData = await response.json();
-  const todoId = responseData.createdTodo.id;
+	const responseData = await response.json();
+	const todoId = responseData.createdTodo.id;
 
-  createTodoListItem(todoText, todoId);
+	createTodoListItem(todoText, todoId);
 }
 
 async function updateTodo(newTodoText) {
-  const todoId = editedTodoElement.dataset.todoid; // data-todoid
-  let response;
+	const todoId = editedTodoElement.dataset.todoid; // data-todoid
+	let response;
 
-  try {
-    response = await fetch('http://localhost:3000/todos/' + todoId, {
-      method: 'PATCH',
-      body: JSON.stringify({
-        newText: newTodoText,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (error) {
-    alert('Something went wrong!');
-    return;
-  }
+	try {
+		response = await fetch('http://localhost:3000/todos/' + todoId, {
+			method: 'PATCH',
+			body: JSON.stringify({
+				newText: newTodoText,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (error) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  if (!response.ok) {
-    alert('Something went wrong!');
-    return;
-  }
+	if (!response.ok) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  editedTodoElement.firstElementChild.textContent = newTodoText;
+	editedTodoElement.firstElementChild.textContent = newTodoText;
 
-  todoFormElement.querySelector('input').value = '';
-  editedTodoElement = null;
+	todoFormElement.querySelector('input').value = '';
+	editedTodoElement = null;
 }
 
 async function deleteTodo(event) {
-  const clickedButtonElement = event.target;
-  const todoElement = clickedButtonElement.parentElement.parentElement;
-  const todoId = todoElement.dataset.todoid;
+	const clickedButtonElement = event.target;
+	const todoElement = clickedButtonElement.parentElement.parentElement;
+	const todoId = todoElement.dataset.todoid;
 
-  let response;
+	let response;
 
-  try {
-    response = await fetch('http://localhost:3000/todos/' + todoId, {
-      method: 'DELETE',
-    });
-  } catch (error) {
-    alert('Something went wrong!');
-    return;
-  }
+	try {
+		response = await fetch('http://localhost:3000/todos/' + todoId, {
+			method: 'DELETE',
+		});
+	} catch (error) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  if (!response.ok) {
-    alert('Something went wrong!');
-    return;
-  }
+	if (!response.ok) {
+		alert('Something went wrong!');
+		return;
+	}
 
-  todoElement.remove();
+	todoElement.remove();
 }
 
 function saveTodo(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  const formInput = new FormData(event.target);
-  const enteredTodoText = formInput.get('text');
+	const formInput = new FormData(event.target);
+	const enteredTodoText = formInput.get('text');
 
-  if (!editedTodoElement) {
-    // We're adding a new todo
-    createTodo(enteredTodoText);
-  } else {
-    // We're updating an existing todo
-    updateTodo(enteredTodoText);
-  }
+	if (!editedTodoElement) {
+		// We're adding a new todo
+		createTodo(enteredTodoText);
+	} else {
+		// We're updating an existing todo
+		updateTodo(enteredTodoText);
+	}
 }
 
 function startTodoEditing(event) {
-  const clickedButtonElement = event.target;
-  editedTodoElement = clickedButtonElement.parentElement.parentElement; // the <li>
-  const currentText = editedTodoElement.firstElementChild.textContent;
+	const clickedButtonElement = event.target;
+	editedTodoElement = clickedButtonElement.parentElement.parentElement; // the <li>
+	const currentText = editedTodoElement.firstElementChild.textContent;
 
-  todoFormElement.querySelector('input').value = currentText;
+	todoFormElement.querySelector('input').value = currentText;
 }
 
 todoFormElement.addEventListener('submit', saveTodo);
