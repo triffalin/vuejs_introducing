0678609fcbfcad9c119b951eebcdc6f1ea44ac42
Alin Triff <triffalin@gmail.com>
2023-10-28 19:23:36 +0300

    loaded data and managed loading states

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 const TodosApp = {
 	data() {
 		return {
+			isLoading: false,
 			todos: [],
 			enteredTodoText: '',
 			editedTodoId: null,
 		};
 	},
 	methods: {
-		saveTodo(event) {
+		async saveTodo(event) {
 			event.preventDefault();
 
 			if (this.editedTodoId) {
@@ -26,10 +27,35 @@ const TodosApp = {
 				this.editedTodoId = null;
 			} else {
 				// Creating...
+				let response;
+
+				try {
+					response = await fetch('http://localhost:3000/todos', {
+						method: 'POST',
+						body: JSON.stringify({
+							text: this.enteredTodoText,
+						}),
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					});
+				} catch (error) {
+					alert('Something went wrong!');
+					return;
+				}
+
+				if (!response.ok) {
+					alert('Something went wrong!');
+					return;
+				}
+
+				const responseData = await response.json();
+
 				const newTodo = {
 					text: this.enteredTodoText,
-					id: new Date().toISOString(),
+					id: responseData.createdTodo.id,
 				};
+
 				this.todos.push(newTodo);
 			}
 
@@ -47,6 +73,27 @@ const TodosApp = {
 				return todoItem.id !== todoId;
 			});
 		},
+	},
+	async created() {
+		let response;
+		this.isLoading = true;
+		try {
+			response = await fetch('http://localhost:3000/todos');
+		} catch (error) {
+			alert('Something went wrong!');
+			this.isLoading = false;
+			return;
+		}
+
+		this.isLoading = false;
+
+		if (!response.ok) {
+			alert('Something went wrong!');
+			return;
+		}
+
+		const responseData = await response.json();
+		this.todos = responseData.todos;
 	},
 };
 
